027a7fad18f25048b518b4a9b61ae23da1f33c9e
znss1989 <znss1989@gmail.com>
2018-07-18 16:43:58 +0800

    get /blocks

--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const Blockchain = require('../blockchain');
+
+const PORT = process.env.PORT || 3001; // set with command like: PORT=3002 npm run dev
+const app = express();
+const blockchain = new Blockchain();
+
+app.get('/blocks', (req, res) => {
+  res.json(blockchain.chain);
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}...`);
+});
--- dev-test.js
+++ dev-test.js
@@ -1,6 +1,5 @@
 const Block = require('./block');
 
-
 const genesisBlock = Block.genesis();
 const fooBlock = Block.mineBlock(Block.genesis(), 'foo');
 console.log(genesisBlock.display());
