91d151980ce61f49b5087a1f2f53b74b4fceed08
znss1989 <znss1989@gmail.com>
2018-07-18 19:34:58 +0800

    post /mine

--- app/index.js
+++ app/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const Blockchain = require('../blockchain');
 
 const PORT = process.env.PORT || 3001; // set with command like: PORT=3002 npm run dev
-const app = express();
 const blockchain = new Blockchain();
+
+const app = express();
+app.use(bodyParser.json());
 
 app.get('/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/mine', (req, res) => {
+  const block = blockchain.addBlock(req.body.data); // validation TBD...
+  console.log(`New block added: ${block.toString()}`);
+  res.redirect('/blocks');
 });
 
 app.listen(PORT, () => {
