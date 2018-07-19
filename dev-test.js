b74b8cd56a0712d01d7c05dbb8d01058620af110
znss1989 <znss1989@gmail.com>
2018-07-19 19:39:08 +0800

    wallet key pair

--- dev-test.js
+++ dev-test.js
@@ -1,7 +1,11 @@
-const Blockchain = require('./blockchain');
+// const Blockchain = require('./blockchain');
 
-const blockchain = new Blockchain();
+// const blockchain = new Blockchain();
 
-for (let i = 0; i < 10; ++i) {
-  console.log(blockchain.addBlock(`foo ${i}`).display());
-}
+// for (let i = 0; i < 10; ++i) {
+//   console.log(blockchain.addBlock(`foo ${i}`).display());
+// }
+
+const Wallet = require('./wallet');
+const wallet = new Wallet();
+console.log(wallet.display());
