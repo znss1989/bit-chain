5f3ae240e71e15808c934497809f6d13982274f6
znss1989 <znss1989@gmail.com>
2018-07-19 16:05:39 +0800

    include and mine nonce value

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,10 +1,13 @@
 const SHA256 = require('crypto-js/sha256');
 
+const {DIFFICULTY} = require('../config');
+
 class Block {
-  constructor(timestamp, lastHash, hash, data) {
+  constructor(timestamp, lastHash, hash, nonce, data) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
+    this.nonce = nonce;
     this.data = data;
   }
 
@@ -13,27 +16,34 @@ class Block {
   Timestamp: ${this.timestamp}
   Last Hash: ${this.lastHash.substring(0, 12)}
   Hash: ${this.hash.substring(0, 12)}
+  Nonce: ${this.nonce}
   data: ${this.data}`;
   }
 
   static genesis() {
-    return new Block('Genesis time', '---', 'f1r57-h45h', []);
+    return new Block('Genesis time', '---', 'f1r57-h45h', 0, []);
   }
 
   static mineBlock(lastBock, data) {
-    const timestamp = Date.now();
+    let timestamp = Date.now();
     const lastHash = lastBock.hash;
-    const hash = Block.hash(timestamp, lastHash, data);
-    return new Block(timestamp, lastHash, hash, data);
+    let nonce = 0;
+    let hash = Block.hash(timestamp, lastHash, nonce, data);
+    while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY)) {
+      ++nonce;
+      timestamp = Date.now();
+      hash = Block.hash(timestamp, lastHash, nonce, data);
+    }
+    return new Block(timestamp, lastHash, hash, nonce, data);
   }
 
-  static hash(timestamp, lastHash, data) {
-    return SHA256(`${timestamp}${lastHash}${data}`).toString();
+  static hash(timestamp, lastHash, nonce, data) {
+    return SHA256(`${timestamp}${lastHash}${nonce}${data}`).toString();
   }
 
   static blockHash(block) {
-    const {timestamp, lastHash, data} = block;
-    return Block.hash(timestamp, lastHash, data);
+    const {timestamp, lastHash, nonce, data} = block;
+    return Block.hash(timestamp, lastHash, nonce, data);
   }
 }
 
---
+++ config.js
@@ -0,0 +1,5 @@
+const DIFFICULTY = 3;
+
+module.exports = {
+  DIFFICULTY
+};
