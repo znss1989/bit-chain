4abea7f5a42df4570e261dc42d3cc253a7f57fed
znss1989 <znss1989@gmail.com>
2018-07-20 14:42:20 +0800

    transaction pool

---
+++ wallet/transaction-pool.js
@@ -0,0 +1,16 @@
+class TransactionPool {
+  constructor() {
+    this.transactions = [];
+  }
+
+  updateOrAddTransaction(transaction) {
+    let transactionMatched = this.transactions.find(t => t.id === transaction.id);
+    if (!transactionMatched) {
+      this.transactions.push(this.transactions);
+      return;
+    }
+    this.transactions[this.transactions.indexOf(transactionMatched)] = transaction; // replace rather than change
+  }
+}
+
+module.exports = TransactionPool;
