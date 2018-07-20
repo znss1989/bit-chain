4eaca3799637d303a7e0a458206036af980be34b
znss1989 <znss1989@gmail.com>
2018-07-20 19:56:09 +0800

    app api for transactions

--- app/index.js
+++ app/index.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001; // set with command like: PORT=3002 npm run dev
 const blockchain = new Blockchain();
+const wallet = new Wallet();
+const transactionPool = new TransactionPool();
 const p2pserver = new P2pServer(blockchain);
 
 const app = express();
@@ -20,6 +24,16 @@ app.post('/mine', (req, res) => {
   console.log(`New block added: ${block.display()}`);
   p2pserver.syncChains();
   res.redirect('/blocks');
+});
+
+app.get('/transactions', (req, res) => {
+  res.json(transactionPool.transactions);
+});
+
+app.post('/transact', (req, res) => {
+  const {recipient, amount} = req.body; // validation TBD...
+  const transaction = wallet.createTransaction(recipient, amount, transactionPool);
+  res.redirect('/transactions');
 });
 
 app.listen(HTTP_PORT, () => {
